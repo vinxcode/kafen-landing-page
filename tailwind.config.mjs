7d1361d23a924935d52cf29f3ea9457bdc96eaa3
Kevin <vincitbussiness@gmail.com>
2024-03-13 11:01:06 -0600

    Agregando Hero, Socialmedia y footer sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'kanit': ['kanit', 'sans-serif']
+		}
 	},
-	plugins: [],
+	plugins: [addDynamicIconSelectors()],
 }
